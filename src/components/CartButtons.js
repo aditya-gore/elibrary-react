10d9e15606ad8ff5bb3cb89c87f9f24094f76120
aditya-gore <aditya_gore@hotmail.com>
2021-05-13 13:58:31 +0530

    login and registration forms added

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -25,7 +25,7 @@ const CartButtons = () => {
         className="auth-btn"
         onClick={closeSideBar}
       >
-        Signup
+        Register
       </Link>
     </Wrapper>
   );
@@ -35,43 +35,12 @@ const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   align-items: center;
-  width: 225px;
+  width: 220px;
+  padding: 20px;
 
-  .cart-btn {
-    color: var(--clr-grey-1);
-    font-size: 1.5rem;
-    letter-spacing: var(--spacing);
-    color: var(--clr-grey-1);
-    display: flex;
-
-    align-items: center;
-  }
-  .cart-container {
-    display: flex;
-    align-items: center;
-    position: relative;
-    svg {
-      height: 1.6rem;
-      margin-left: 5px;
-    }
-  }
-  .cart-value {
-    position: absolute;
-    top: -10px;
-    right: -16px;
-    background: var(--clr-primary-5);
-    width: 16px;
-    height: 16px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    font-size: 0.75rem;
-    color: var(--clr-white);
-    padding: 12px;
-  }
   .auth-btn {
     display: flex;
+    margin: 20px;
     align-items: center;
     background: transparent;
     border-color: transparent;
@@ -79,8 +48,8 @@ const Wrapper = styled.div`
     cursor: pointer;
     color: var(--clr-grey-1);
     letter-spacing: var(--spacing);
-    svg {
-      margin-left: 5px;
+    &:hover {
+      border-bottom: 2px solid var(--clr-primary-7);
     }
   }
 `;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import "./index.css";
 import App from "./App";
 import { BooksProvider } from "./context/books_context";
 import { FilterProvider } from "./context/filter_context";
-
+import axios from "axios";
 import { UserProvider } from "./context/user_context";
+
+axios.defaults.baseURL = "http://localhost:8000/api/";
+axios.defaults.withCredentials = true;
 
 ReactDOM.render(
   <BooksProvider>
